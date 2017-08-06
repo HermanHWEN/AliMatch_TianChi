df2a30e659c85c750e9c3d491ee42f4a9ab92340
Administrator <Administrator@Herman>
2017-08-06 15:42:56 +0800

    Update holiday

--- src/main/java/calculateFeatures/Holiday.java
+++ src/main/java/calculateFeatures/Holiday.java
@@ -19,27 +19,17 @@ public class Holiday {
 
 		{
 			add(new String[]{"2016-01-01", "2016-01-03"});
-			add(new String[]{"2016-02-06"});
 			add(new String[]{"2016-02-07", "2016-02-13"});
-			add(new String[]{"2016-02-14"});
 			add(new String[]{"2016-04-02", "2016-04-04"});
 			add(new String[]{"2016-04-30", "2016-05-02"});
 			add(new String[]{"2016-06-09", "2016-06-11"});
-			add(new String[]{"2016-06-12"});
 			add(new String[]{"2016-09-15", "2016-09-17"});
-			add(new String[]{"2016-09-18"});
 			add(new String[]{"2016-10-01", "2016-10-07"});
-			add(new String[]{"2016-10-08", "2016-10-09"});
 			add(new String[]{"2016-12-31", "2017-01-02"});
-			add(new String[]{"2017-01-22"});
 			add(new String[]{"2017-01-27", "2017-02-02"});
-			add(new String[]{"2017-02-04"});
-			add(new String[]{"2017-04-01"});
 			add(new String[]{"2017-04-02", "2017-04-04"});
 			add(new String[]{"2017-04-29", "2017-05-01"});
-			add(new String[]{"2017-05-27"});
 			add(new String[]{"2017-05-28", "2017-05-30"});
-			add(new String[]{"2017-09-30"});
 			add(new String[]{"2017-10-01", "2017-10-08"});
 		}
 	};
--- src/main/java/controll/Constant.java
+++ src/main/java/controll/Constant.java
@@ -19,7 +19,7 @@ public class Constant {
 	public final static int SIZE_OF_DATA=-1;
 	public final static int REPEATE_TIMES=10000000;//for gradient descend
 	public final static double LEARNING_RATE=0.1;//for gradient descend
-	public final static double THREDHOLD=0.001;//for gradient descend
+	public final static double THREDHOLD=0.00001;//for gradient descend
 	
 	
 	//gradient descend

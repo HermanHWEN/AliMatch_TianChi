384a2673da4afe449ce1f8cb9ec2bc9f96ccf83e
Administrator <Administrator@Herman>
2017-08-06 21:58:15 +0800

    Update comments

--- src/main/java/controll/Constant.java
+++ src/main/java/controll/Constant.java
@@ -15,9 +15,9 @@ public class Constant {
 	public final static String PATH_OF_MODEL="C://Match//{1}//Model_{0}_{1}.txt";
 	
 	public final static int MAXORDER=4;
-	public final static int FOLDTIME=10;//for cross validation
-	public final static int LINES_NEED_TO_READ=-1;
-	public final static int SIZE_OF_TRAINING_DATA=1000;
+	public final static int FOLDTIME=10;//fold time of cross validation
+	public final static int LINES_NEED_TO_READ=-1;//how many lines of txt file need to be read in.
+	public final static int SIZE_OF_TRAINING_DATA=1000;//how many data for training
 	public final static double LEARNING_RATE=0.25;//initial learning rate for gradient descend
 	public final static double LEARNING_RATE_DIVISOR=2;//if the error is going up,learning rate will divided by this
 	public final static double LEARNING_RATE_LBOUND_DIVISOR=10240;//if the current learning rate<(initial/this), will stop
@@ -27,10 +27,10 @@ public class Constant {
 	
 	//gradient descend
 	public final static int MAX_REPEATE_TIMES=100000;//for gradient descend
-	public final static boolean MUST_REACH_MAX_TIME=false;//false, will use pseudo inverse
+	public final static boolean MUST_REACH_MAX_TIME=false;//true, num of epochs must be MAX_REPEATE_TIMES;false, will do the early-stopping by LEARNING_RATE_LBOUND_DIVISOR
 	public final static boolean USE_GRADIENT_DESCEND=true;//false, will use pseudo inverse
-	public final static boolean USE_STOCHASTIC_GRADIENT_DESCEND=true;//false, will use small batch stochastic gradient descend
-	public final static int SIZE_OF_ONE_BATCH=200;
+	public final static boolean USE_STOCHASTIC_GRADIENT_DESCEND=true;//true, will use small batch stochastic gradient descend;false will use whole data set
+	public final static int SIZE_OF_ONE_BATCH=200;//when USE_STOCHASTIC_GRADIENT_DESCEND is true, using it as batch size
 	
 	//normalization
 	public final static boolean USE_MIN_MAX_NORMALIZATION=false;

f69ac85816810940d92308f393314cd2915c520c
HH <1427112351@qq.com>
2017-08-08 19:10:49 +0800

    Tuning

--- src/main/java/controll/Constant.java
+++ src/main/java/controll/Constant.java
@@ -16,16 +16,15 @@ public class Constant {
 	public final static int ACURACY_OF_TRAVEL_TIME_OUTPUT=14;//accuracy of travel time for output
 	
 	public final static int MAXORDER=2;
-	public final static int MINORDER_PER_FIELD=-2;
 	public final static int FOLDTIME=10;//fold time of cross validation
 	public final static int LINES_NEED_TO_READ=-1;//how many lines of txt file need to be read in.
 	public final static int SIZE_OF_TRAINING_DATA=5000;//how many data for training
-	public final static double LEARNING_RATE=0.25;//initial learning rate for gradient descend
+	public final static double LEARNING_RATE=2;//initial learning rate for gradient descend
 	public final static boolean DECAY_LEARNING_RATE=true;
 	public final static double LEARNING_RATE_DIVISOR=2;//if the error is going up,learning rate will divided by this
-	public final static double LEARNING_RATE_LBOUND_DIVISOR=Math.pow(2, 10);//if the current learning rate<(initial/this), will stop
+	public final static double LEARNING_RATE_LBOUND_DIVISOR=Math.pow(2, 13);//if the current learning rate<(initial/this), will stop
 	public final static int MAX_NUM_OF_EPOCH=1000;//for when epoch time reach this, will check if error is “no-improvement-in-n"
-	public final static int ACURACY_OF_ERROR=5;//accuracy of error when checking min error
+	public final static int ACURACY_OF_ERROR=6;//accuracy of error when checking min error
 //	public final static double THRESHOLD=0.00001;//for gradient descend
 	
 	
